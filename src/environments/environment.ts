bd1c2d3a294459c1a58bbc514f0fff53fadac60b
Abdul Rahman <abdul.095722@gmail.com>
2019-07-13 18:44:59 +0500

    Complete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://route123.herokuapp.com',
   production: false
 };
 
